aa1aaa3e5c7bfd32a82d7ea24493709773869a02
Abel <abel.liu.china@outlook.com>
2016-03-30 17:32:27 +0800

    set

--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
-﻿$(function(){
-    $('#img_sync').click(function () {
-        doa();
+﻿syncing = false;
+
+$(function() {
+	loadLastSyncTime();
+	
+    $('#img_sync').click(function() {
+        if (!syncing) {
+            doSync();
+        }
     });
 });
 
+function doSync() {
+    syncing = true;
+    $('#img_sync').addClass('animated infinite rotateIn');
+    showMsg("正在同步...");
+}
 
+function endRotate() {
+    $('#img_sync').removeClass('animated infinite rotateIn');
+}
+
+function loadLastSyncTime() {
+    showMsg("最后同步时间：<br />2016-3-20 15:22");
+}
+
+function showMsg(msg) {
+    $("#msg").html(msg);
+}
